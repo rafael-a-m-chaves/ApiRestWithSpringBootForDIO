66f53f570d34c48efb5018e4aa8f1f8b5b9219dc
Rafael Chaves <rafaelaugustochaves@hotmail.com>
2021-07-18 10:59:23 -0300

    implement Interface Ocorrencia

--- src/main/java/com/dio/projetoSpring/repository/OcorrenciaRepository.java
+++ src/main/java/com/dio/projetoSpring/repository/OcorrenciaRepository.java
@@ -1,4 +1,10 @@
 package com.dio.projetoSpring.repository;
 
-public interface OcorrenciaRepository {
+import com.dio.projetoSpring.model.Ocorrencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OcorrenciaRepository extends JpaRepository<Ocorrencia,Long> {
+
 }
