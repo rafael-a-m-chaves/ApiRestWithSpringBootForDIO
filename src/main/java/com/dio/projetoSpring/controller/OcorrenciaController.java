ea5d5cdd7fab9aa647c9c37a4700c3e012936ccb
Rafael Chaves <rafaelaugustochaves@hotmail.com>
2021-07-18 11:18:50 -0300

    implement constructor for Class Controller for model Ocorrencia

--- src/main/java/com/dio/projetoSpring/controller/OcorrenciaController.java
+++ src/main/java/com/dio/projetoSpring/controller/OcorrenciaController.java
@@ -1,4 +1,16 @@
 package com.dio.projetoSpring.controller;
 
+import com.dio.projetoSpring.service.OcorrenciaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ocorrencia")
 public class OcorrenciaController {
+    OcorrenciaService ocorrenciaService
+    @Autowired
+    public OcorrenciaController(OcorrenciaService ocorrenciaService) {
+        this.ocorrenciaService = ocorrenciaService;
+    }
 }
