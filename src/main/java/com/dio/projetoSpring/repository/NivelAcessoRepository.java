bc727b7c23f76b346ac75923ab4ab18522bbccd2
Rafael Chaves <rafaelaugustochaves@hotmail.com>
2021-07-18 14:18:18 -0300

    Create and implement interface Repository for Model class NivelAcesso

---
+++ src/main/java/com/dio/projetoSpring/repository/NivelAcessoRepository.java
@@ -0,0 +1,7 @@
+package com.dio.projetoSpring.repository;
+
+import com.dio.projetoSpring.model.NivelAcesso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NivelAcessoRepository extends JpaRepository<NivelAcesso,Long> {
+}
