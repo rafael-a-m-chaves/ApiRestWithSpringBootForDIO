5d957f500a65a957041a8a0d5d12d0a1869aff20
Rafael Chaves <rafaelaugustochaves@hotmail.com>
2021-07-18 01:12:37 -0300

    creation of the controller class of the DataType model and implementation of method post

--- src/main/java/com/dio/projetoSpring/controller/TipoDataController.java
+++ src/main/java/com/dio/projetoSpring/controller/TipoDataController.java
@@ -1,2 +1,27 @@
-package com.dio.projetoSpring.controller;public class TipoDataController {
+package com.dio.projetoSpring.controller;
+
+import com.dio.projetoSpring.model.TipoData;
+import com.dio.projetoSpring.service.TipoDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tipoData")
+public class TipoDataController {
+
+    TipoDataService tipoDataService;
+    @Autowired //Constructor
+    public TipoDataController(TipoDataService tipoDataService)
+    {
+        this.tipoDataService = tipoDataService;
+    }
+
+
+    @PostMapping
+    public TipoData save(@RequestBody TipoData tipoData){
+        return tipoDataService.save(tipoData);
+    }
 }
