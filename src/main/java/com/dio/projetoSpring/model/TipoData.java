0d61c898aae151345e533045c1776d89cdf55a84
Rafael Chaves <rafaelaugustochaves@hotmail.com>
2021-07-18 00:53:16 -0300

    Melhoria na Classe model Tipo Data

--- src/main/java/com/dio/projetoSpring/model/TipoData.java
+++ src/main/java/com/dio/projetoSpring/model/TipoData.java
@@ -3,6 +3,7 @@ package com.dio.projetoSpring.model;
 import lombok.*;
 import org.hibernate.envers.Audited;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -13,9 +14,9 @@ import javax.persistence.Id;
 @EqualsAndHashCode
 @Builder
 @Entity
-
 public class TipoData {
     @Id
     private Long id;
+    @Column
     private String descricao;
 }
