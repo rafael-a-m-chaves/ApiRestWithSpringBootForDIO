8b39edd37dc8ec502cd729dda7fb4b871b6a4dcd
Rafael Chaves <rafaelaugustochaves@hotmail.com>
2021-07-18 13:46:21 -0300

    Method Post implement in class controller of model Ocorrencia

--- src/main/java/com/dio/projetoSpring/controller/OcorrenciaController.java
+++ src/main/java/com/dio/projetoSpring/controller/OcorrenciaController.java
@@ -1,16 +1,24 @@
 package com.dio.projetoSpring.controller;
 
+import com.dio.projetoSpring.model.Ocorrencia;
 import com.dio.projetoSpring.service.OcorrenciaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ocorrencia")
 public class OcorrenciaController {
-    OcorrenciaService ocorrenciaService
+    OcorrenciaService ocorrenciaService;
     @Autowired
     public OcorrenciaController(OcorrenciaService ocorrenciaService) {
         this.ocorrenciaService = ocorrenciaService;
+    }
+
+    @PostMapping
+    public Ocorrencia save(@RequestBody Ocorrencia ocorrencia){
+        return ocorrenciaService.save(ocorrencia);
     }
 }
