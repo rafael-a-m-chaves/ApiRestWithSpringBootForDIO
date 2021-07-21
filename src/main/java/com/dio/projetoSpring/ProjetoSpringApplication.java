019754f443a85c9e1834a477b6f93635630052d6
Rafael Chaves <rafaelaugustochaves@hotmail.com>
2021-07-21 10:31:06 -0300

    Implemention of page web index and improve in log e controllers

--- src/main/java/com/dio/projetoSpring/ProjetoSpringApplication.java
+++ src/main/java/com/dio/projetoSpring/ProjetoSpringApplication.java
@@ -1,13 +1,19 @@
 package com.dio.projetoSpring;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjetoSpringApplication {
 
+
 	public static void main(String[] args) {
+		Logger logger = LogManager.getLogger(ProjetoSpringApplication.class);
+		logger.debug("teste");
 		SpringApplication.run(ProjetoSpringApplication.class, args);
+
 	}
 
 }
---
+++ src/main/java/com/dio/projetoSpring/controller/Index.java
@@ -0,0 +1,16 @@
+package com.dio.projetoSpring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class Index {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+}
