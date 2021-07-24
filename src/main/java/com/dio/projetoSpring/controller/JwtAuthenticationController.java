c68a27ff9ec2570bf2c44e865f47df091a5ec653
Rafael Chaves <rafaelaugustochaves@hotmail.com>
2021-07-24 10:34:07 -0300

    improve and config jwt Class's

--- src/main/java/com/dio/projetoSpring/controller/JwtAuthenticationController.java
+++ src/main/java/com/dio/projetoSpring/controller/JwtAuthenticationController.java
@@ -1,4 +1,4 @@
-package com.dio.projetoSpring.configJWT;
+package com.dio.projetoSpring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
