238f6b89b7c61de34bc7bfa84ba6f1914a7775a4
Rafael Chaves <rafaelaugustochaves@hotmail.com>
2021-07-27 15:32:23 -0300

    Update template index

---
+++ src/main/resources/static/js/jsPrincipal.js
@@ -0,0 +1,40 @@
+
+function enviar2(){
+    var user = document.getElementById("username");
+    var pass = document.getElementById("password")
+    const data =  {username:user.value, password:pass.value};
+    fetch("/", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+        })
+        .then((response) => response.json())
+        .then((data1) => {
+            console.log("Success:", data1);
+            window.localStorage.setItem('token',data1.token);
+            return redirecionar();
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+    });
+
+}
+function redirecionar(){
+    fetch("/tipodata/all", {
+        method: "GET",
+        headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + window.localStorage.getItem('token'),
+        },
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Success:", data);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+    });
+};
+    
