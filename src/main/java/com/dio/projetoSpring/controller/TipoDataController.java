afaf05d08059971eab124eb7c0675ce581fb100e
Rafael Chaves <rafaelaugustochaves@hotmail.com>
2021-07-27 15:08:49 -0300

    update class TipoData Controller

--- src/main/java/com/dio/projetoSpring/controller/TipoDataController.java
+++ src/main/java/com/dio/projetoSpring/controller/TipoDataController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/tipoData")
+@RequestMapping("/tipodata")
 public class TipoDataController {
 
     TipoDataService tipoDataService;
