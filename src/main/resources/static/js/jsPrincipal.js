e11df8d9ae3981d26900b3ea17eea956d97800c5
Rafael Chaves <rafaelaugustochaves@hotmail.com>
2021-07-29 00:41:31 -0300

    improve jsPrincipal

--- src/main/resources/static/js/jsPrincipal.js
+++ src/main/resources/static/js/jsPrincipal.js
@@ -9,34 +9,57 @@ function enviar2(){
         },
         body: JSON.stringify(data),
         })
-        .then((response) => response.json())
-        .then((data1) => {
-            window.localStorage.setItem('token',data1.token);
-            return redirecionar();
-        })
+        .then(function(response){
+            if (response.status !=200){
+                var lblErrorUserPass = document.getElementById("lblErrorUserPass");
+                $(lblErrorUserPass).show();
+            }else{
+                var lblErrorUserPass = document.getElementById("lblErrorUserPass");
+                $(lblErrorUserPass).hide();
+                response.json()
+                .then((data1) => {
+                window.localStorage.setItem('token',data1.token);
+                return window.location.href ="http://localhost:8081/hub"
+                
+            })
+            
+        }
+        }) 
         .catch((error) => {
             console.error("Error:", error);
     });
 
 }
-function redirecionar(){
-    fetch("/hub", {
+async function redirecionar(){
+    let html ="";
+    let log22 = await fetch("/hub", {
         method: "GET",
         headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + window.localStorage.getItem('token'),
+            "X-Content-Type-Options":"nosniff",
+            "X-XSS-Protection":"1; mode=block",
+            "X-Frame-Options":"DENY",
+            "Content-Type": "text/html;charset=UTF-8",
+            "Content-Language":"pt-BR",
+            "Transfer-Encoding":"chunked",
+            "Authorization": "Bearer " + window.localStorage.getItem('token'),
         },
         })
-        .then((response) => {
-            let reader = response.body.getReader();
-            reader.read().then(function(data){  
-                document.open();
-                document.write(String.fromCharCode.apply(null, data.value));
+        .then(function(response){
+
+            if (response.status != 200){
+                alert("Problema Para Carregar os Dados");
+                return;
+            }
+    
+            response.text().then(function(text){
+                html = text;
+                console.log(html);
             });
-            
         })
         .catch((error) => {
             console.error("Error:", error);
     });
+
+    
 };
     
